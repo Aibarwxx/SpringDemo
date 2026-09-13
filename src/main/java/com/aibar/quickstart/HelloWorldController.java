7b168e76f0270bbea09269e4325967066d2a1fe7
Aibar <ajbarmadiuly@gmail.com>
2026-09-13 18:45:39 +0900

    Add hello page test

--- src/main/java/com/aibar/quickstart/HelloWorldController.java
+++ src/main/java/com/aibar/quickstart/HelloWorldController.java
@@ -1,4 +1,14 @@
 package com.aibar.quickstart;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class HelloWorldController {
+
+    @GetMapping(path = "/hello")
+    public String helloWorld(){
+        return "First time learning spring kinda nervous";
+    }
 }
